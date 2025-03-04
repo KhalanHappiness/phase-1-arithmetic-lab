6454b84c806b56a54a96b771a3ed99b37100cd97
happiness <happinesskhalan@gmail.com>
2025-03-05 01:04:14 +0300

    Adds the javascript arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+let multiply = num1 * num2
+
+console.log(multiply)
+
+let random = Math.floor(Math.random()*10)+1
+
+console.log(random)
+
+const num3 = 40;
+const num4 = 9;
+
+
+let mod = num3 % num4
+
+console.log(mod)
+
+
+
+let max = Math.max(1,2 ,3, 20, 6 , 7, 10)
+
+console.log(max)
